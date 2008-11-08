2234289d4d63aa8515ad7202cdff5f822e12161d
Adam J. Forster <adam@adamforster.org>
2008-11-08 20:45:16 +0000

    * Refactored code to be object oriented.
    git-svn-id: https://lightsy.googlecode.com/svn@6 7fb402fe-a8fc-11dd-9f94-e5dfbee8a4f1

--- trunk/lightsy/main.py
+++ trunk/lightsy/main.py
@@ -1,94 +1,98 @@
-from random import randint, choice
-
 import pyglet
 from pyglet.window import mouse
 
 from levels import LEVELS
 
-LIGHT_WIDTH = 50
-LIGHT_HEIGHT = 45
-BOARD_SIZE = 5
+class Board():
+    SIZE = 5
+    LIGHT_WIDTH = 50
+    LIGHT_HEIGHT = 45
 
-EMPTY = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-]
-
-def build_board(random=False):
-    '''
-    Example board:
-    board = [
-        [1, 0, 0, 0, 1],
-        [0, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0],
-        [1, 0, 0, 0, 1],
-    ]
-    '''
-    if random:
-        board = []
-        for y in range(BOARD_SIZE):
-            row = []
-            for x in range(BOARD_SIZE):
-                row.append(randint(0, 1))
-            board.append(row)
-    else:
-        board = choice(LEVELS)
-    return board
-
-board = build_board()
-
-window = pyglet.window.Window(BOARD_SIZE * LIGHT_WIDTH, BOARD_SIZE * LIGHT_HEIGHT)
-window.set_caption('Lightsy')
-light_off = pyglet.resource.image('light-off.jpg')
-light_on = pyglet.resource.image('light-on.jpg')
-
-def draw_board():
-    for row in range(BOARD_SIZE):
-        for square in range(BOARD_SIZE):
-            if board[row][square] == 1:
-                light_on.blit(LIGHT_WIDTH * square, window.height - (LIGHT_HEIGHT * row) - LIGHT_HEIGHT)
-            else:
-                light_off.blit(LIGHT_WIDTH * square, window.height - (LIGHT_HEIGHT * row) - LIGHT_HEIGHT)
-
-def x_index(x):
-    return x//LIGHT_WIDTH
-  
-def y_index(y):
-    return (y//LIGHT_HEIGHT - 4) * -1
-
-def change_lights(x, y):
-    global board
+    LIGHT_ON = pyglet.resource.image('light-on.jpg')
+    LIGHT_OFF = pyglet.resource.image('light-off.jpg')
     
-    x = x_index(x)
-    y = y_index(y)
-    coord_set = (
-        (y-1, x),
-        (y, x-1),
-        (y, x),
-        (y, x+1),
-        (y+1, x),
-    )
-    for coords in coord_set:
-        try:
-            if coords[0] >= 0 and coords[1] >= 0:
-                board[coords[0]][coords[1]] = not(board[coords[0]][coords[1]])
-        except IndexError:
-            pass
-    if board == EMPTY:
-        board = build_board()
+    EMPTY = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    
+    def __init__(self):
+        self.load_level(0)
+        
+    def load_level(self, level_number):
+        # Ensure that self.lights is a clone of the level and not an alias.
+        self.lights = LEVELS[level_number][:]
+        self.current_level = level_number
+    
+    def is_complete(self):
+        if self.lights == Board.EMPTY:
+            return True
+        else:
+            return False
+    
+    def change_level(self, level_number):
+        self.load_level(level_number)
+    
+    def __get_x_index(self, x):
+        return x//Board.LIGHT_WIDTH
+      
+    def __get_y_index(self, y):
+        return (y//Board.LIGHT_HEIGHT - 4) * -1
+    
+    def draw(self, max_height):
+        for row in range(Board.SIZE):
+            for square in range(Board.SIZE):
+                if self.lights[row][square] == 1:
+                    Board.LIGHT_ON.blit(
+                        Board.LIGHT_WIDTH * square,
+                        max_height - (Board.LIGHT_HEIGHT * row) - Board.LIGHT_HEIGHT
+                    )
+                else:
+                    Board.LIGHT_OFF.blit(
+                        Board.LIGHT_WIDTH * square,
+                        max_height - (Board.LIGHT_HEIGHT * row) - Board.LIGHT_HEIGHT
+                    )
+    
+    def press(self, x, y):
+        x = self.__get_x_index(x)
+        y = self.__get_y_index(y)
+        coordinate_set = (
+            (y-1, x),
+            (y, x-1),
+            (y, x),
+            (y, x+1),
+            (y+1, x),
+        )
+        for coords in coordinate_set:
+            try:
+                if coords[0] >= 0 and coords[1] >= 0:
+                    self.lights[coords[0]][coords[1]] = not(self.lights[coords[0]][coords[1]])
+            except IndexError:
+                pass
+        if self.is_complete():
+            self.change_level(self.current_level + 1)
+    
+class LightsyWindow(pyglet.window.Window):
+    def __init__(self):
+        super(LightsyWindow, self).__init__()
+        self.board = Board()
+        self.set_caption('Lightsy')
+        self.set_size(
+            Board.SIZE * Board.LIGHT_WIDTH,
+            Board.SIZE * Board.LIGHT_HEIGHT
+        )
+    
+    def on_draw(self):
+        self.clear()
+        self.board.draw(self.height)
+        
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == mouse.LEFT:
+            self.board.press(x, y)
 
-@window.event
-def on_draw():
-    window.clear()
-    draw_board()
-
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    if button == mouse.LEFT:
-        change_lights(x, y)
-
-pyglet.app.run()
+if __name__ == '__main__':
+    LightsyWindow()
+    pyglet.app.run()
